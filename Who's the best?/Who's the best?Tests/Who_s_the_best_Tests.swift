c08fc7c7a76f6359027c057ba8cf42ae1f1574d9
Caio-MacBook <caioa.morais@gmail.com>
2024-12-28 18:53:40 -0300

    YOU DA BESTgit add .git add .

---
+++ Who's the best?/Who's the best?Tests/Who_s_the_best_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Who_s_the_best_Tests.swift
+//  Who's the best?Tests
+//
+//  Created by Caio Araujo on 23/12/24.
+//
+
+import Testing
+@testable import Who_s_the_best_
+
+struct Who_s_the_best_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
